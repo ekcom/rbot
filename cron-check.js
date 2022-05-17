aaac6b29f340f476dd60abfa81af4124129952b8
Elijah Mock <92Eli@users.noreply.github>
2022-05-17 18:24:47 -0500

    patch - deal with errors more

--- cron-check.js
+++ cron-check.js
@@ -2,16 +2,21 @@ const setCronAlarm = require("./setCronAlarm");
 
 
 async function check() {
-    const result = await setCronAlarm();
-    
-    // console.log(result);
-    if (result.close === false) {
-        // get it on the next pass
-        console.log("Shutting down due to time being far off...");
-        process.kill(process.pid, "SIGTERM"); // "SIGHUP"
-    } else {
-        console.log("Keeping server awake because it is almost time...");
-        // it will be past time. wait
+    try {
+        const result = await setCronAlarm();
+        
+        // console.log(result);
+        if (result.close === false) {
+            // get it on the next pass
+            console.log("Shutting down due to time being far off...");
+            process.kill(process.pid, "SIGTERM"); // "SIGHUP"
+        } else {
+            console.log("Keeping server awake because it is almost time...");
+            // it will be past time. wait
+        }
+    } catch (err) {
+        console.error("[cron-check] Error starting cron:", err);
+        reply("When waking up to check the time, we could not properly schedule the reminder. It may or may not work today.");
     }
 }
 
