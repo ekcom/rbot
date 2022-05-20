26fecb1df47b5bcc87c1090e11333826bca26b73
Elijah Mock <92Eli@users.noreply.github>
2022-05-20 12:53:32 -0500

    update time display message

--- receiveMessage.js
+++ receiveMessage.js
@@ -249,9 +249,11 @@ function setTime(pgClient, timeStringPlusJunk) {
     setConfigTo(pgClient, { hourToSend: hour, minuteToSend: minute })
         .then(json => {
             let amPm = "am";
-            if (hour > 12) {
+            if (hour >= 12) {
                 amPm = "pm";
-                hour = hour-12; // for our purposes now
+                if (hour !== 12) hour = hour-12; // for our purposes now
+            } else if (hour === 0) {
+                hour = 12; // to display (12am)
             }
             if (minute < 10) {
                 minute += "0"; // a string now
